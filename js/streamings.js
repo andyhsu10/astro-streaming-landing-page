0114bbe54c051c440288cb9d298fea19baa5802a
An-Ting Hsu <andyhsu10@gmail.com>
2020-06-09 19:35:09 +0800

    feat: update demo streamings

--- js/streamings.js
+++ js/streamings.js
@@ -1,110 +1,58 @@
 streamings = [
     {
         "id": 1,
-        "location_name": "EBC 東森新聞",
-        "latitude": null,
-        "longitude": null,
-        "selected": false,
-        "url": "https://www.youtube.com/watch?v=RaIJ767Bj_M",
-        "youtube_id": "RaIJ767Bj_M"
+        "location_name": "中央氣象局2019年12月26日日食直播",
+        "selected": true,
+        "url": "https://www.youtube.com/watch?v=4fbz9GuOtF0",
+        "youtube_id": "4fbz9GuOtF0"
     },
     {
         "id": 2,
-        "location_name": "公共電視 網路直播 PTS Live",
-        "latitude": null,
-        "longitude": null,
+        "location_name": "中央氣象局2018年7月28日月全食直播",
         "selected": true,
-        "url": "https://www.youtube.com/watch?v=ED4QXd5xAco",
-        "youtube_id": "ED4QXd5xAco"
+        "url": "https://www.youtube.com/watch?v=BcEHjCuvzoQ",
+        "youtube_id": "BcEHjCuvzoQ"
     },
     {
         "id": 3,
-        "location_name": "三立LIVE新聞HD直播",
-        "latitude": null,
-        "longitude": null,
+        "location_name": "2018年1月31日月全食(中央氣象局)",
         "selected": false,
-        "url": "https://www.youtube.com/watch?v=4ZVUmEUFwaY",
-        "youtube_id": "4ZVUmEUFwaY"
+        "url": "https://www.youtube.com/watch?v=4AC9VFdXFyM",
+        "youtube_id": "4AC9VFdXFyM"
     },
     {
         "id": 4,
-        "location_name": "The Good Life Radio",
-        "latitude": null,
-        "longitude": null,
+        "location_name": "2016年3月9日日偏食直播(中央氣象局)",
         "selected": false,
-        "url": "https://www.youtube.com/watch?v=36YnV9STBqc",
-        "youtube_id": "36YnV9STBqc"
+        "url": "https://www.youtube.com/watch?v=jOBeiuh30wE",
+        "youtube_id": "jOBeiuh30wE"
     },
     {
         "id": 5,
-        "location_name": "Chillhop Radio",
-        "latitude": null,
-        "longitude": null,
-        "selected": true,
-        "url": "https://www.youtube.com/watch?v=5yx6BWlEVcY",
-        "youtube_id": "5yx6BWlEVcY"
+        "location_name": "2015年4月4日月全食直播(中央氣象局)",
+        "selected": false,
+        "url": "https://www.youtube.com/watch?v=Jau1TROqaNE",
+        "youtube_id": "Jau1TROqaNE"
     },
     {
         "id": 6,
-        "location_name": "【豆漿 - SoybeanMilk】每張醜照　都是嘔心瀝血之作",
-        "latitude": null,
-        "longitude": null,
-        "selected": true,
-        "url": "https://www.youtube.com/watch?v=0T8VZgm_Vc4",
-        "youtube_id": "0T8VZgm_Vc4"
+        "location_name": "C/2014 Q2 (Lovejoy)彗星",
+        "selected": false,
+        "url": "https://www.youtube.com/watch?v=GwC3Qt6ceU4",
+        "youtube_id": "GwC3Qt6ceU4"
     },
     {
         "id": 7,
-        "location_name": "【豆漿 - SoybeanMilk】俊榮本人帶俊榮割蛋蛋",
-        "latitude": null,
-        "longitude": null,
-        "selected": true,
-        "url": "https://www.youtube.com/watch?v=F9Ax9_vWkQQ",
-        "youtube_id": "F9Ax9_vWkQQ"
+        "location_name": "2011年6月16日月全食(望遠鏡直焦)",
+        "selected": false,
+        "url": "https://www.youtube.com/watch?v=aqevf_QFlQ8",
+        "youtube_id": "aqevf_QFlQ8"
     },
     {
         "id": 8,
-        "location_name": "【豆漿 - SoybeanMilk】家裡遭小偷了",
-        "latitude": null,
-        "longitude": null,
-        "selected": true,
-        "url": "https://www.youtube.com/watch?v=EXc-xtpmxak",
-        "youtube_id": "EXc-xtpmxak"
-    },
-    {
-        "id": 9,
-        "location_name": "【豆漿 - SoybeanMilk】崩潰　一天跑了三間不同的醫院",
-        "latitude": null,
-        "longitude": null,
-        "selected": true,
-        "url": "https://www.youtube.com/watch?v=8p7vUU3OmSI",
-        "youtube_id": "8p7vUU3OmSI"
-    },
-    {
-        "id": 10,
-        "location_name": "【豆漿 - SoybeanMilk】我家的貓會飛",
-        "latitude": null,
-        "longitude": null,
-        "selected": true,
-        "url": "https://www.youtube.com/watch?v=TGaXkro56qk",
-        "youtube_id": "TGaXkro56qk"
-    },
-    {
-        "id": 11,
-        "location_name": "【豆鬥榮 EP05】盡釋前嫌　豆漿與俊榮終於破冰了？",
-        "latitude": null,
-        "longitude": null,
-        "selected": true,
-        "url": "https://www.youtube.com/watch?v=SfQDbFYvig0",
-        "youtube_id": "SfQDbFYvig0"
-    },
-    {
-        "id": 12,
-        "location_name": "【豆漿 - SoybeanMilk】我花3個小時剪一隻貓",
-        "latitude": null,
-        "longitude": null,
-        "selected": true,
-        "url": "https://www.youtube.com/watch?v=5Rq8xh2U-z8",
-        "youtube_id": "5Rq8xh2U-z8"
+        "location_name": "2011年6月16日月全食(中央氣象局)",
+        "selected": false,
+        "url": "https://www.youtube.com/watch?v=JvrLS4JDTr0",
+        "youtube_id": "JvrLS4JDTr0"
     },
 ]
