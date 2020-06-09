340cd634fb999ac766c852f0c111c67518eddd9e
An-Ting Hsu <andyhsu10@gmail.com>
2020-06-09 18:26:19 +0800

    feat: RWD

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
-var selectOptionNumber = 4;
+var selectOptionNumber = 0;
 $(document).ready(function() {
+    setSelectOptionNumber();
+    $(window).on('resize', function(){
+        setSelectOptionNumber();
+        adjustStreamingsHeight();
+    });
+
     $('#video_multiselect').multiselect({
-        buttonWidth: '250px',
         nonSelectedText: '請選擇至少一個頻道',
         onChange: function(element, insert) {
             id = element[0].value
@@ -11,54 +16,71 @@ $(document).ready(function() {
             } else {
                 removeStreaming(id);
             }
-
-            // Get selected options.
-            var selectedOptions = $('#video_multiselect option:selected');
- 
-            if (selectedOptions.length >= selectOptionNumber) {
-                // Disable all other checkboxes.
-                var nonSelectedOptions = $('#video_multiselect option').filter(function() {
-                    return !$(this).is(':selected');
+            disableOptions();
+        },
+        buttonText: function(options, select) {
+            if (options.length === 0) {
+                return '請選擇至少一個頻道';
+            } else if (options.length > 3) {
+                return '已選擇 ' + options.length + ' 個頻道';
+            } else {
+                var labels = [];
+                options.each(function() {
+                    if ($(this).attr('label') !== undefined) {
+                        labels.push($(this).attr('label'));
+                    } else {
+                        labels.push($(this).html());
+                    }
                 });
-
-                nonSelectedOptions.each(function() {
-                    var input = $('input[value="' + $(this).val() + '"]');
-                    input.prop('disabled', true);
-                    input.closest('li').addClass('disabled');
-                });
-            }
-            else {
-                // Enable all checkboxes.
-                $('#video_multiselect option').each(function() {
-                    var input = $('input[value="' + $(this).val() + '"]');
-                    input.prop('disabled', false);
-                    input.closest('li').removeClass('disabled');
-                });
-            }
+                return labels.join(', ') + '';
+             }
         }
     });
 
-    $('#video_multiselect').multiselect('dataprovider', getStreaminOptions(streamings));
+    $('#video_multiselect').multiselect('dataprovider', getStreamingOptions(streamings));
     selected_options = $('#video_multiselect option:selected').map(function(a, item){return item.value;});
-    for (var i = 0; i < selected_options.length; i++) {
+    for (var i = 0; i < selected_options.length && i < selectOptionNumber; i++) {
         const streaming = findStreaming(selected_options[i]);
         appendStreaming(streaming);
     }
+
+    disableOptions();
+    adjustStreamingsHeight();
 });
 
-function getStreaminOptions(streamings) {
-    return streamings.map(function(item){
+function setSelectOptionNumber() {
+    if ($(window).width() >= 768) {
+        selectOptionNumber = 9
+    } else {
+        // switch mobile screen
+        if (selectOptionNumber == 9) {
+            mobileStreamings()
+        }
+        selectOptionNumber = 2
+    }
+    disableOptions();
+}
+
+function getStreamingOptions(streamings) {
+    count = 0
+    streamingOptions = streamings.map(function(item){
+        if (item.selected) {
+            count += 1;
+        }
         return {
             label: item.location_name,
             title: item.location_name,
             value: item.id,
-            selected: item.selected
+            selected: count <= selectOptionNumber ? item.selected : false,
         }
     })
+
+    return streamingOptions;
 }
 
 function appendStreaming(streaming) {
-    $('#streaming_list').append('<div class="col-lg-4 streaming-video mb-4" id="streaming_video_'+streaming.id+'"><iframe id="player" width=100% src="https://www.youtube.com/embed/'+streaming.youtube_id+'?autoplay=1&mute=1&info=0&controls=0" frameborder="0"></iframe></div>')
+    $('#streaming_list').append('<div class="col-lg-4 col-md-6 streaming-video mb-4" id="streaming_video_'+streaming.id+'"><iframe id="player" width=100% height=100% src="https://www.youtube.com/embed/'+streaming.youtube_id+'?autoplay=0&mute=1&info=0&controls=0" frameborder="0"></iframe></div>');
+    adjustStreamingsHeight();
 }
 
 function removeStreaming(id) {
@@ -67,4 +89,47 @@ function removeStreaming(id) {
 
 function findStreaming(id) {
     return streamings.find(element => element.id == id);
+}
+
+function mobileStreamings() {
+    ids = []
+    $('.streaming-video').each(function(index, value) {
+        if (index > 1) {
+            $(this).remove();
+            ids.push($(this)[0].id.split('_')[2]);
+        }
+    });
+    $('#video_multiselect').multiselect('deselect', ids);
+}
+
+function disableOptions() {
+    // Get selected options.
+    var selectedOptions = $('#video_multiselect option:selected');
+ 
+    if (selectedOptions.length >= selectOptionNumber) {
+        // Disable all other checkboxes.
+        var nonSelectedOptions = $('#video_multiselect option').filter(function() {
+            return !$(this).is(':selected');
+        });
+
+        nonSelectedOptions.each(function() {
+            var input = $('input[value="' + $(this).val() + '"]');
+            input.prop('disabled', true);
+            input.closest('li').addClass('disabled');
+        });
+    }
+    else {
+        // Enable all checkboxes.
+        $('#video_multiselect option').each(function() {
+            var input = $('input[value="' + $(this).val() + '"]');
+            input.prop('disabled', false);
+            input.closest('li').removeClass('disabled');
+        });
+    }
+}
+
+function adjustStreamingsHeight() {
+    $('.streaming-video').each(function() {
+        $(this).height($(this).width() / 16 * 9);
+    });
 }
--- js/streamings.js
+++ js/streamings.js
@@ -44,4 +44,67 @@ streamings = [
         "url": "https://www.youtube.com/watch?v=5yx6BWlEVcY",
         "youtube_id": "5yx6BWlEVcY"
     },
+    {
+        "id": 6,
+        "location_name": "【豆漿 - SoybeanMilk】每張醜照　都是嘔心瀝血之作",
+        "latitude": null,
+        "longitude": null,
+        "selected": true,
+        "url": "https://www.youtube.com/watch?v=0T8VZgm_Vc4",
+        "youtube_id": "0T8VZgm_Vc4"
+    },
+    {
+        "id": 7,
+        "location_name": "【豆漿 - SoybeanMilk】俊榮本人帶俊榮割蛋蛋",
+        "latitude": null,
+        "longitude": null,
+        "selected": true,
+        "url": "https://www.youtube.com/watch?v=F9Ax9_vWkQQ",
+        "youtube_id": "F9Ax9_vWkQQ"
+    },
+    {
+        "id": 8,
+        "location_name": "【豆漿 - SoybeanMilk】家裡遭小偷了",
+        "latitude": null,
+        "longitude": null,
+        "selected": true,
+        "url": "https://www.youtube.com/watch?v=EXc-xtpmxak",
+        "youtube_id": "EXc-xtpmxak"
+    },
+    {
+        "id": 9,
+        "location_name": "【豆漿 - SoybeanMilk】崩潰　一天跑了三間不同的醫院",
+        "latitude": null,
+        "longitude": null,
+        "selected": true,
+        "url": "https://www.youtube.com/watch?v=8p7vUU3OmSI",
+        "youtube_id": "8p7vUU3OmSI"
+    },
+    {
+        "id": 10,
+        "location_name": "【豆漿 - SoybeanMilk】我家的貓會飛",
+        "latitude": null,
+        "longitude": null,
+        "selected": true,
+        "url": "https://www.youtube.com/watch?v=TGaXkro56qk",
+        "youtube_id": "TGaXkro56qk"
+    },
+    {
+        "id": 11,
+        "location_name": "【豆鬥榮 EP05】盡釋前嫌　豆漿與俊榮終於破冰了？",
+        "latitude": null,
+        "longitude": null,
+        "selected": true,
+        "url": "https://www.youtube.com/watch?v=SfQDbFYvig0",
+        "youtube_id": "SfQDbFYvig0"
+    },
+    {
+        "id": 12,
+        "location_name": "【豆漿 - SoybeanMilk】我花3個小時剪一隻貓",
+        "latitude": null,
+        "longitude": null,
+        "selected": true,
+        "url": "https://www.youtube.com/watch?v=5Rq8xh2U-z8",
+        "youtube_id": "5Rq8xh2U-z8"
+    },
 ]
