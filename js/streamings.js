8e4f8af24d7b3b377e6508eec3db6e378bcd4904
An-Ting Hsu <andyhsu10@gmail.com>
2020-06-20 12:02:55 +0800

    update streaming list

--- js/streamings.js
+++ js/streamings.js
@@ -1,51 +1,121 @@
 const streamings = [
     {
         "id": 1,
-        "location_name": "2020年6月21日日環食直播(中央氣象局@局本部天文臺)",
-        "selected": true,
-        "url": "https://youtu.be/jT40XoeMr0U",
-        "youtube_id": "jT40XoeMr0U"
-    },
-    {
-        "id": 2,
         "location_name": "2020年6月21日日環食直播(中央氣象局@吉貝國小)",
         "selected": true,
         "url": "https://youtu.be/lSZ5DpeeO8Y",
         "youtube_id": "lSZ5DpeeO8Y"
     },
     {
+        "id": 2,
+        "location_name": "2020年6月21日日環食直播(中央氣象局@局本部天文臺)",
+        "selected": true,
+        "url": "https://youtu.be/jT40XoeMr0U",
+        "youtube_id": "jT40XoeMr0U"
+    },
+    {
         "id": 3,
+        "location_name": "全國大學天文社聯盟-中山醫天文@中山醫學大學",
+        "selected": false,
+        "url": "https://youtu.be/MjRDyILN0yQ",
+        "youtube_id": "MjRDyILN0yQ"
+    },
+    {
+        "id": 4,
+        "location_name": "全國大學天文社聯盟-中華大學天文社 @中華大學",
+        "selected": false,
+        "url": "https://www.youtube.com/watch?v=GGCU9qmOaZA",
+        "youtube_id": "GGCU9qmOaZA"
+    },
+    {
+        "id": 5,
+        "location_name": "全國大學天文社聯盟－國立東華大學天文社 @ 富北國中",
+        "selected": false,
+        "url": "https://youtu.be/_xBwrMHAK9E",
+        "youtube_id": "_xBwrMHAK9E"
+    },
+    {
+        "id": 6,
+        "location_name": "全國大學天文社聯盟 @ 雲林台西鄉",
+        "selected": false,
+        "url": "https://youtu.be/QBviSi80NEQ",
+        "youtube_id": "QBviSi80NEQ"
+    },
+    {
+        "id": 7,
+        "location_name": "全國大學天文社聯盟 @ 雲林縣口湖鄉",
+        "selected": false,
+        "url": "https://youtu.be/hpSZ8EzSpPw",
+        "youtube_id": "hpSZ8EzSpPw"
+    },
+    {
+        "id": 8,
+        "location_name": "全國大學天文社聯盟-高師天文@高師燕巢校區",
+        "selected": false,
+        "url": "https://youtu.be/nvkZSVbLRHo",
+        "youtube_id": "nvkZSVbLRHo"
+    },
+    {
+        "id": 9,
+        "location_name": "國立嘉義女中日環食即時串流@嘉義女中",
+        "selected": false,
+        "url": "https://youtu.be/epnUmPG9NG0",
+        "youtube_id": "epnUmPG9NG0"
+    },
+    {
+        "id": 10,
+        "location_name": "國立嘉義高級中學 - 日環食即時串流",
+        "selected": false,
+        "url": "https://www.youtube.com/channel/UCnF-5kVCbNm6wGIryj0qymQ/live",
+        "youtube_id": "ufowAy830Z8"
+    },
+    {
+        "id": 11,
+        "location_name": "日環食直播@高雄市中山高中 20200621",
+        "selected": false,
+        "url": "https://youtu.be/owBhcEqXbXo",
+        "youtube_id": "owBhcEqXbXo"
+    },
+    {
+        "id": 12,
+        "location_name": "日環食金門縣金城國中天文台",
+        "selected": false,
+        "url": "https://youtu.be/9u42gWcMKls",
+        "youtube_id": "9u42gWcMKls"
+    },
+    {
+        "id": 13,
+        "location_name": "暨大附中天文台觀測隊",
+        "selected": false,
+        "url": "https://youtu.be/PJH0N6BeEh4",
+        "youtube_id": "PJH0N6BeEh4"
+    },
+    {
+        "id": 14,
+        "location_name": "清大天文台",
+        "selected": false,
+        "url": "https://youtu.be/KUvCpuAMnlI",
+        "youtube_id": "KUvCpuAMnlI"
+    },
+    {
+        "id": 15,
+        "location_name": "澎湖「話說日環蝕彩繪漁翁島 」活動",
+        "selected": false,
+        "url": "https://youtu.be/tsGs7ef3FAE",
+        "youtube_id": "tsGs7ef3FAE"
+    },
+    {
+        "id": 16,
         "location_name": "臺北天文館日偏食直播",
         "selected": true,
         "url": "https://www.youtube.com/watch?v=PMs12Je6qhw",
         "youtube_id": "PMs12Je6qhw"
     },
     {
-        "id": 4,
-        "location_name": "雲林五條港安西府廟前廣場",
-        "selected": false,
-        "url": "https://youtu.be/k2fXEeEqqHM",
-        "youtube_id": "k2fXEeEqqHM"
-    },
-    {
-        "id": 5,
-        "location_name": "清大天文台",
-        "selected": false,
-        "url": "https://youtu.be/blbju552t2I",
-        "youtube_id": "blbju552t2I"
-    },
-    {
-        "id": 6,
-        "location_name": "豐中天文台",
+        "id": 17,
+        "location_name": "豐原高中天文台",
         "selected": false,
         "url": "https://youtu.be/vbO1KMM5CEs",
         "youtube_id": "vbO1KMM5CEs"
-    },
-    {
-        "id": 7,
-        "location_name": "2020富北國中日環食直播",
-        "selected": true,
-        "url": "https://youtu.be/LvFU2-OrH0U",
-        "youtube_id": "LvFU2-OrH0U"
-    },
+    }
 ]
